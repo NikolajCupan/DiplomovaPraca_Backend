61812649970f5e0b7938ecdd41f48a84fe91cce8
NikolajCupan <cupannikolaj@gmail.com>
2024-10-20 10:55:31 +0200

    Init project

---
+++ src/test/java/com/backend/thesis/ThesisApplicationTests.java
@@ -0,0 +1,11 @@
+package com.backend.thesis;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ThesisApplicationTests {
+    @Test
+    void contextLoads() {
+    }
+}
