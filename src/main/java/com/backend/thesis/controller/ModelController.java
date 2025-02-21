365858106652bccd7b0f31c28eb32de9fb634910
NikolajCupan <cupannikolaj@gmail.com>
2025-02-21 13:50:20 +0100

    Simple exponential smoothing

--- src/main/java/com/backend/thesis/controller/ModelController.java
+++ src/main/java/com/backend/thesis/controller/ModelController.java
@@ -72,6 +72,38 @@ public class ModelController {
     }
 
     @CrossOrigin(exposedHeaders = Constants.SESSION_COOKIE_NAME)
+    @PostMapping(path = "/model/simple-exp-smoothing")
+    public ResponseEntity<Type.RequestResult<String>> handleSimpleExpSmoothing(
+            final HttpServletRequest request,
+            @RequestParam(name = "idDataset") final String idDataset,
+            @RequestParam(name = "train_percent") final String trainPercent,
+            @RequestParam(name = "alpha") final String alpha,
+            @RequestParam(name = "forecast_count") final String forecastCount
+    ) {
+        final Type.ActionResult<DatasetEntity> datasetResult = this.datasetService.getDatasetOfUser(
+                request.getAttribute(Constants.SESSION_COOKIE_NAME).toString(),
+                Helper.stringToLong(idDataset)
+        );
+
+        if (!datasetResult.isSuccess()) {
+            return new ResponseEntity<>(new Type.RequestResult<>(datasetResult.message(), null), HttpStatus.BAD_REQUEST);
+        }
+
+        final Type.ActionResult<JSONObject> result = this.modelService.simpleExpSmoothing(
+                datasetResult.data(),
+                Helper.stringToLong(trainPercent),
+                Helper.stringToDouble(alpha),
+                Helper.stringToLong(forecastCount)
+        );
+
+        if (result.isSuccess()) {
+            return new ResponseEntity<>(new Type.RequestResult<>(result.message(), result.data().toString()), HttpStatus.OK);
+        } else {
+            return new ResponseEntity<>(new Type.RequestResult<>(result.message(), null), result.getHttpStatus());
+        }
+    }
+
+    @CrossOrigin(exposedHeaders = Constants.SESSION_COOKIE_NAME)
     @PostMapping(path = "/model/holt-winter")
     public ResponseEntity<Type.RequestResult<String>> handleHoltWinterModel(
             final HttpServletRequest request,
--- src/main/java/com/backend/thesis/utility/python/PythonConstants.java
+++ src/main/java/com/backend/thesis/utility/python/PythonConstants.java
@@ -54,5 +54,6 @@ public class PythonConstants {
 
 
     public static final String ACTION_ARIMA = "arima";
+    public static final String ACTION_SIMPLE_EXP_SMOOTHING = "simple_exp_smoothing";
     public static final String ACTION_HOLT_WINTER = "holt_winter";
 }
